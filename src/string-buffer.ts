e0f14628cd0b0b185911796590b6821401149cf1
Aditi Polkam <aditipolkam@gmail.com>
2024-01-05 18:55:47 +0530

    fix: return types of string-buffer conversions

--- src/string-buffer.ts
+++ src/string-buffer.ts
@@ -1,15 +1,15 @@
-export function bufferToUtf8String(buffer: Uint8Array) {
+export function bufferToUtf8String(buffer: Uint8Array) : string {
     return Buffer.from(buffer).toString('utf-8');
 }
 
-export function utf8StringToBuffer(hexString: string) {
+export function utf8StringToBuffer(hexString: string) : Uint8Array {
     return Buffer.from(hexString, 'utf-8');
 }
 
-export function bufferToHexString(buffer: Uint8Array) {
+export function bufferToHexString(buffer: Uint8Array) : string {
     return Buffer.from(buffer).toString('hex');
 }
 
-export function hexStringToBuffer(hexString: string) {
+export function hexStringToBuffer(hexString: string) : Uint8Array {
     return Buffer.from(hexString, 'hex');
 }
